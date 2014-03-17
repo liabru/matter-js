00feed5272f1de34829df34e075d997ec9a13d42
liabru <liambrum@gmail.com>
2014-03-17 17:22:49 +0000

    prevented possible redundant object initialisation

--- src/core/Events.js
+++ src/core/Events.js
@@ -42,7 +42,9 @@ var Events = {};
             eventClone;
 
         if (object.events) {
-            event = event || {};
+            if (!event)
+                event = {};
+
             names = eventNames.split(' ');
 
             for (var i = 0; i < names.length; i++) {
