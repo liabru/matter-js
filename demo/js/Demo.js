419a4300ccb6de883be3ba498f7d4ba9c2b36973
liabru <liambrum@gmail.com>
2014-03-28 23:52:51 +0000

    added catapult demo

--- demo/js/Demo.js
+++ demo/js/Demo.js
@@ -496,6 +496,32 @@
         renderOptions.showAngleIndicator = true;
     };
 
+    Demo.catapult = function() {
+        var _world = _engine.world;
+        
+        Demo.reset();
+
+        var stack = Composites.stack(250, 255, 1, 6, 0, 0, function(x, y, column, row) {
+            return Bodies.rectangle(x, y, 30, 30);
+        });
+
+        var catapult = Bodies.rectangle(400, 520, 320, 20, {  });
+
+        World.add(_world, [
+            stack,
+            catapult,
+            Bodies.rectangle(250, 555, 20, 50, { isStatic: true }),
+            Bodies.circle(560, 100, 50, { density: 0.005 }),
+            Constraint.create({ bodyA: catapult, pointB: { x: 390, y: 580 } }),
+            Constraint.create({ bodyA: catapult, pointB: { x: 410, y: 580 } })
+        ]);
+        
+        var renderOptions = _engine.render.options;
+        renderOptions.showCollisions = true;
+        renderOptions.showVelocity = true;
+        renderOptions.showAngleIndicator = true;
+    };
+
     Demo.beachBalls = function() {
         var _world = _engine.world;
         
