e22ceebed52d2c93f02972c42ae145ee61c65726
liabru <liabru@brm.io>
2014-06-21 22:39:55 +0100

    added docs for engine.timing.isFixed

--- src/core/Engine.js
+++ src/core/Engine.js
@@ -545,6 +545,16 @@ var Engine = {};
      */
 
     /**
+     * A `Boolean` that specifies if the `Engine.run` game loop should use a fixed timestep (otherwise it is variable).
+     * If timing is fixed, then the apparant simulation speed will change depending on the frame rate (but behaviour will be deterministic).
+     * If the timing is variable, then the apparant simulation speed will be constant (approximately, but at the cost of determininism).
+     *
+     * @property timing.isFixed
+     * @type boolean
+     * @default false
+     */
+
+    /**
      * A `Number` that specifies the time step between updates in milliseconds.
      * If `engine.timing.isFixed` is set to `true`, then `delta` is fixed.
      * If it is `false`, then `delta` can dynamically change to maintain the correct apparant simulation speed.
