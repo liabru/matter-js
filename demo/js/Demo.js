4051823f5f8d6a4f5bbddb2111d9331a14f43994
liabru <liabru@brm.io>
2014-07-30 17:27:42 +0100

    added Demo.compositeManipulation

--- demo/js/Demo.js
+++ demo/js/Demo.js
@@ -293,6 +293,48 @@
         renderOptions.showCollisions = true;
     };
 
+    Demo.compositeManipulation = function() {
+        var _world = _engine.world;
+
+        Demo.reset();
+
+        var stack = Composites.stack(200, 200, 4, 4, 0, 0, function(x, y, column, row) {
+            return Bodies.rectangle(x, y, 40, 40);
+        });
+
+        World.add(_world, stack);
+
+        _world.gravity.y = 0;
+
+        _sceneEvents.push(
+            Events.on(_engine, 'tick', function(event) {
+                var time = _engine.timing.timestamp;
+
+                Composite.translate(stack, {
+                    x: Math.sin(time * 0.001) * 2,
+                    y: 0
+                });
+
+                Composite.rotate(stack, Math.sin(time * 0.001) * 0.01, {
+                    x: 300,
+                    y: 300
+                });
+
+                var scale = 1 + (Math.sin(time * 0.001) * 0.01);
+
+                Composite.scale(stack, scale, scale, {
+                    x: 300,
+                    y: 300
+                });
+            })
+        );
+
+        var renderOptions = _engine.render.options;
+        renderOptions.wireframes = false;
+        renderOptions.showAxes = true;
+        renderOptions.showCollisions = true;
+    };
+
     Demo.views = function() {
         var _world = _engine.world;
         
