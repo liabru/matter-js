37a86cd321d0ab1d6f9ca46b5505b03c24bae5e5
liabru <liabru@brm.io>
2015-12-23 13:42:22 +0000

    updated edge build

--- build/matter.js
+++ build/matter.js
@@ -612,7 +612,7 @@ var Axes = require('../geometry/Axes');
         body.force.x += force.x;
         body.force.y += force.y;
         var offset = { x: position.x - body.position.x, y: position.y - body.position.y };
-        body.torque += (offset.x * force.y - offset.y * force.x) * body.inverseInertia;
+        body.torque += offset.x * force.y - offset.y * force.x;
     };
 
     /**
