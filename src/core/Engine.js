e2e7c0b10d8b983fc9acef32be0ad7782da0f032
liabru <liabru@brm.io>
2016-04-18 00:22:09 +0100

    fix Matter.Engine.create docs to show element param is optional

--- src/core/Engine.js
+++ src/core/Engine.js
@@ -32,7 +32,7 @@ var Body = require('../body/Body');
      * All properties have default values, and many are pre-calculated automatically based on other properties.
      * See the properties section below for detailed information on what you can pass via the `options` object.
      * @method create
-     * @param {HTMLElement} element
+     * @param {HTMLElement} [element]
      * @param {object} [options]
      * @return {engine} engine
      */
