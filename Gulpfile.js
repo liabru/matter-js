0cf97f5c3c06c2622c37469428deecb206221c47
liabru <liabru@brm.io>
2018-06-11 19:05:09 +0100

    fixed require for external libraries, closes #629, closes #559, closes #593, closes #365, closes #629

--- Gulpfile.js
+++ Gulpfile.js
@@ -111,13 +111,16 @@ gulp.task('watch', function() {
     var b = browserify({
         entries: ['src/module/main.js'],
         standalone: 'Matter',
-        plugin: [watchify],
-        transform: ['browserify-shim']
+        plugin: [watchify]
     });
 
     var bundle = function() {
         gutil.log('Updated bundle build/matter-dev.js');
         b.bundle()
+            .on('error', function(err) {
+                gutil.log('ERROR', err.message);
+                this.emit('end');
+            })
             .pipe(through2({ objectMode: true }, function(chunk, encoding, callback) {
                 return callback(
                     null, 
--- src/factory/Bodies.js
+++ src/factory/Bodies.js
@@ -18,7 +18,7 @@ var Common = require('../core/Common');
 var Body = require('../body/Body');
 var Bounds = require('../geometry/Bounds');
 var Vector = require('../geometry/Vector');
-var decomp = require('poly-decomp');
+var decomp;
 
 (function() {
 
@@ -197,6 +197,10 @@ var decomp = require('poly-decomp');
      * @return {body}
      */
     Bodies.fromVertices = function(x, y, vertexSets, options, flagInternal, removeCollinear, minimumArea) {
+        if (!decomp) {
+            decomp = Common._requireGlobal('decomp', 'poly-decomp');
+        }
+
         var body,
             parts,
             isConvex,
