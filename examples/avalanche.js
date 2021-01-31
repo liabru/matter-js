1ba125582f289ea4cb96fc62ad9624602eda619b
liabru <liabru@brm.io>
2021-01-31 21:51:09 +0000

    catch missing plugin require in examples

--- examples/avalanche.js
+++ examples/avalanche.js
@@ -1,12 +1,16 @@
 var Example = Example || {};
 
 Example.avalanche = function() {
-    if (typeof MatterWrap !== 'undefined') {
-        // either use by name from plugin registry (Browser global)
-        Matter.use('matter-wrap');
-    } else {
-        // or require and use the plugin directly (Node.js, Webpack etc.)
-        Matter.use(require('matter-wrap'));
+    try {
+        if (typeof MatterWrap !== 'undefined') {
+            // either use by name from plugin registry (Browser global)
+            Matter.use('matter-wrap');
+        } else {
+            // or require and use the plugin directly (Node.js, Webpack etc.)
+            Matter.use(require('matter-wrap'));
+        }
+    } catch (e) {
+        // could not require the plugin or install needed
     }
     
     var Engine = Matter.Engine,
