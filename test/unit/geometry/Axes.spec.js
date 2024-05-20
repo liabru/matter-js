b363eb3f809b5488803b9dfeec026aa1c6841f62
gsenden <gsenden@outlook.com>
2024-05-20 11:41:47 +0200

    Axes.rotate unit tests

--- test/unit/geometry/Axes.spec.js
+++ test/unit/geometry/Axes.spec.js
@@ -31,3 +31,48 @@ describe('Axes.fromVertices', () => {
   
     });
 });
+
+describe('Axes.rotate', () => { 
+    it('should be able to rotate axes from valid axes', () => {
+        // Arrange
+		const axes = Axes.fromVertices(getTestVerticesSqaureWithoutBody());
+		const angle = 90.;
+		    
+        // Act
+        Axes.rotate(axes, angle);
+    
+		// Assert
+		assertXY(axes[0], -0.8939966636005579, -0.4480736161291702);
+		assertXY(axes[1], 0.4480736161291702, -0.8939966636005579);
+  
+	});
+	
+	it('should not be able to rotate axes from valid axes and undefined angle', () => {
+        // Arrange
+		const axes = Axes.fromVertices(getTestVerticesSqaureWithoutBody());
+		const angle = undefined;
+		    
+        // Act
+        Axes.rotate(axes, angle);
+    
+		// Assert
+		// TODO: This causes the result to be NaN. This probaby should be fixed.
+		assertXY(axes[0], NaN, NaN );
+		assertXY(axes[1], NaN, NaN );
+  
+    });
+
+    it('should not be able to rotate axes from undefined axes', () => {
+        // Arrange
+		const axes = undefined;
+		const angle = 90.;
+    
+        // Act
+        const result = () => Axes.rotate(axes, angle);
+    
+        // Assert
+        // TODO: This causes a read from undefined. This should probably be fixed.
+        expect(result).toThrow("Cannot read properties of undefined (reading 'length')");
+  
+    });
+});
