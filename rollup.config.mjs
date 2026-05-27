69d776b8a1e21517c338ab79a0605c6f63a494fb
liabru <liabru@brm.io>
2026-05-27 12:54:19 +0100

    migrate to pnpm and rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -16,51 +16,12 @@ const dev = process.env.ROLLUP_WATCH === 'true' || process.env.NODE_ENV === 'dev
 const name = 'matter-demo.bundle';
 const outDir = path.resolve(__dirname, (dev ? 'build' : 'demo'));
 const devPath = './src/module/main.js';
-const devServer = true;
 
-const banner =
-  `/*! ${name} ${pkg.version} by @liabru
-/* ${pkg.homepage}
-/* License ${pkg.license}
+const banner = `/*! 
+* ${name} ${pkg.version} by @liabru
+* ${pkg.homepage}
+* License ${pkg.license}
 */`;
-
-function vendorCommentsPlugin() {
-  return {
-    renderChunk(code, chunk) {
-      let bundleString = code;
-
-      for (const [id, moduleInfo] of Object.entries(chunk.modules)) {
-        let vendor = null;
-
-        if (moduleInfo.renderedLength === 0 || !moduleInfo.code) continue;
-        if (id.includes('?')) continue;
-
-        if (id.includes('.pnpm/')) {
-          vendor = id.split('.pnpm/')[1].split('/')[0];
-        } else if (id.includes('node_modules/')) {
-          vendor = id.split('node_modules/')[1].split('/')[0];
-        }
-
-        if (!vendor) continue;
-
-        const moduleCode = moduleInfo.code;
-        const start = bundleString.indexOf(moduleCode);
-
-        if (start !== -1) {
-          const end = start + moduleCode.length;
-          bundleString = 
-            bundleString.slice(0, start) + 
-            `\n\n/*! Vendor chunk: ${vendor} \n*/\n\n` + 
-            moduleCode + 
-            `\n\n/*! End of vendor chunk. \n*/\n\n` + 
-            bundleString.slice(start + moduleCode.length);
-        }
-      }
-
-      return { code: bundleString, map: null };
-    }
-  };
-}
 
 export default {
   input: { [name]: 'demo/src/index.js' },
@@ -93,12 +54,11 @@ export default {
       preventAssignment: true,
       values: {
         __MATTER_VERSION__: JSON.stringify('*'),
-        __MATTER_IS_DEV__: JSON.stringify(devServer)
+        __MATTER_IS_DEV__: JSON.stringify(dev)
       }
     }),
     resolve({ browser: true }),
     commonjs(),
-    !dev && vendorCommentsPlugin(),
     !dev && terser({
       mangle: true,
       format: {
